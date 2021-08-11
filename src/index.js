b1350599eddbcd33dafd280d89c52f1518af6015
Luis Almeida <luisjalmeidad@gmail.com>
2021-08-11 12:49:52 -0300

    Curso finalizado sin Cloud

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import { obtenerUsuarios } from "./js/http-provider";
+//import { obtenerUsuarios } from "./js/http-provider";
 // import { init } from "./js/chistes-page";
-import { init } from "./js/usuarios-page";
-
-
+//import { init } from "./js/usuarios-page";
 // obtenerChiste().then( console.log );
-
 // obtenerUsuarios().then( console.log );
-
-init();
-
+// init();
 
 
 
+// CRUD
+
+import * as CRUD from './js/crud-provider';
+
+
+// CRUD.getUsuario( 2 ).then( console.log );
+
+// CRUD.crearUsuario({
+//     name: 'Luis',
+//     job: 'Carpintero'
+// }).then( console.log );
+
+
+// CRUD.actualizarUsuario( 1, {
+//     name: 'Luis',
+//     job: 'Developer'
+// }).then( console.log );
+
+CRUD.borrarUsuario( 1 ).then( console.log );
 
